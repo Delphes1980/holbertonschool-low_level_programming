ae3670ba0a22d9774a55a48e4a9f48fe7a406f04
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-02-18 12:39:12 +0100

    alphABET

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -7,12 +7,12 @@
 int main(void)
 {
 	char alphabet = 'a';
-	char upper_alphabet = toupper(alphabet)
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		putchar(alphabet);
 
-		putchar(upper_alphabet);
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+		putchar(alphabet)
 
 	putchar('\n');
 		return (0);
