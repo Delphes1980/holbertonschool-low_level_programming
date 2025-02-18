55f07983f736a7f3d6da705024e01fc7c20997ef
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-02-18 15:23:39 +0100

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int suite
+
+	for (suite = '0'; suite < 10; suite++)
+		putchar(suite +'0');
+
+	putchar(',');
+
+	putchar(' ');
+
+	putchar('\n');
+
+	return (0);
+}
