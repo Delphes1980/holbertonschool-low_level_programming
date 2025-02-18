309bafa4d21f7f72f6ee62c7c18e8bf0b1fa6b12
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-02-18 15:51:17 +0100

    Patience, persistence and perspiration make an unbeatable combination for success

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,17 +9,17 @@ int main(void)
 int suite;
 
 	for (suite = 0; suite < 10; suite++)
-{
+
 	if (suite == 9)
-{
+
 	continue;
-}
+
 		putchar(suite);
-}
+
 
 	putchar(',');
 
-	putchar ();
+	putchar (' ');
 
 	putchar('\n');
 		return (0);
