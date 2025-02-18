f0254a5a4da391fe76553e97d4c02396f128aa6a
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-02-18 14:38:11 +0100

    Hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 30; numbers <= 39; numbers++)
+		putchar(numbers);
+
+	putchar('\n');
+	return (0);
+}
