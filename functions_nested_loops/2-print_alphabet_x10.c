67585e9014b11c73e673cf736cc317b3e5d74529
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-02-19 14:05:24 +0100

    10 x alphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,22 @@
 #"include "main.h"
 
 /**
- * print_alphabet_x10 - check the code
- *
- * Return: Always 0.
- */
+* print_alphabet_x10 - check the code
+*
+* Return: Always 0.
+*/
 
 void print_alphabet_x10(void)
 {
-	int x = 1;
+	int x = 0;
 	char alphabet_x10;
 
 	while (x < 10)
 	{
 		for (alphabet_x10 = 'a'; alphabet_x10 <= 'z'; alphabet_x10++)
+		{
 			_putchar(alphabet_x10);
+		}
 	}
 	_putchar('\n');
 	x++;
