dea56e29f9f36b8c741b1460f862776e7c072f0b
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-02-19 16:35:19 +0100

    Sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* print_sign - check the code
+* @n: character to check
+* Return: 1 if n greater than 0 and prints +
+* 0 if n = 0 and prints 0
+* -1 if n is less than 0 and prints -
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+
+	else if (n < 0)
+	{
+		_putchar('-');
+	}
+
+	return ('-1');
+}
