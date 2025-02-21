a1db4cffdeb091687d9e684f74c8f92fb7c4175b
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-02-21 12:02:15 +0100

    98 Battery Street, the OG

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_to_98 - prints all natural numbers from n to 98
+* @n: the number that begins the list
+*
+* Return: Always 0
+*/
+
+void print_to_98(int n)
+{
+
+	for (n <= 0; n <= 98; n++)
+	{
+
+		if (n != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+
+		_putchar(n);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
