9dc0fa9bfef6fa46b254ec70602f8787db4571ae
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-02-21 09:26:53 +0100

    Learn your times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,16 +10,25 @@ void times_table(void)
 {
 	int num1, num2;
 
-	for (num1 = '48'; num1 <= '57'; num1++)
+	for (num1 = 0; num1 <= 9; num1++)
 	{
 
-		for (num2 = '48'; num2 <= '57'; num2++)
+		for (num2 = 0; num2 <= 9; num2++)
 		{
 			int table = num1 * num2;
 
 			_putchar(table + '0');
+
+			if (table > 9)
+			{
+				_putchar((table / 10) + '0');
+				_putchar((table % 10) + '0');
+			}
+			else
+			{
 			_putchar(',');
 			_putchar('.');
+			}
 		}
 
 			_putchar('\n');
