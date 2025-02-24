b0c3f994346df3cbd2835a32b701f7e8701ae894
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-02-24 16:36:03 +0100

    You are so much sunshine in every square inch

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_scare - prints a square
+* @size: size of the square
+*
+* Return: nothing
+*/
+
+void print_square(int size)
+{
+	while (n-- > 0)
+	{
+		_putchar('#');
+
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}
