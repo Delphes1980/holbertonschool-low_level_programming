1ed7916c04b0fc7fa9012a43f4ab6bae39489d26
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-02-24 16:48:04 +0100

    You are so much sunshine in every square inch

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -10,13 +10,15 @@
 void print_square(int size)
 {
 	int i, j;
+	char sign = '#';
 
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
 		{
-			_putchar("#");
+			_putchar(sign);
 		}
+	}
 
 	_putchar('\n');
 }
