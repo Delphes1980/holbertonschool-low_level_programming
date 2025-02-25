cd85f3da46f06492068bd2e37c221cf1548ee3dc
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-02-25 10:48:20 +0100

    98 Battery st.

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* reset_to_98 - updates the value to 98
+* @n: parameter
+*
+* Return: nothing
+*/
+
+void reset_to_98(int *n)
+{
+
+	int f;
+	int *n;
+
+	f = 402;
+	n = &f;
+
+	_putchar(f);
+
+	*n = 98;
+
+	_putchar(&f);
+
+}
