2ef76a21c600e51c77b042a4d0497ba9e76f28b7
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-02-25 19:04:21 +0100

    Half the lies they tell about me aren't true

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* puts2 - print every character of a spring, starting with the first one
+* @str: parameter
+*
+* Return: nothing
+*/
+
+void puts2(char *str)
+{
+	int n;
+
+	n = 0;
+
+	while (str[n] != '\0')
+	{
+		n++;
+	}
+
+	if (n % 2 == 0)
+	{
+		_putchar(str);
+	}
+
+	_putchar('\n');
+
+}
