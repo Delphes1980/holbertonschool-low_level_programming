097528c45d581e99a94cbddcc4a8246f20e2cf32
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-02-25 14:02:13 +0100

    I do not fear computers. I fear the lack of them

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _puts - prints a string follow by a new line
+*@str: parameter
+*
+* Return: nothing
+*/
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; *str != '\0'; str++)
+	{
+		i++;
+	}
+
+	return (i);
+
+	_putchar('\n');
+
+}
