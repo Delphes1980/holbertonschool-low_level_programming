f004cac3928c8a18b9df347e56dd275ea6d53e69
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-02-28 10:05:05 +0100

    strcmp

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+* _strcmp - compares 2 strings
+* @s1: first string to be compared
+* @s2: second string to be compared
+*
+* Return: an integer
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+
+	int i = 0;
+	int j = 0;
+
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+
+	if (s1[i] > s2[j])
+	{
+		return (1);
+	}
+
+	else if (s1[i] == '\0' && s2[j] == '\0')
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (-1);
+	}
+}
