6482862962bb6725b711f85a4617b5083274ffe5
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-02-28 14:29:56 +0100

    Always look up

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* string_toupper - changes all lowercase to uppercase
+* @*: parameter
+*
+* Return: char
+*/
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = (int)str[i] - 32;
+		}
+
+	return (str);
+}
