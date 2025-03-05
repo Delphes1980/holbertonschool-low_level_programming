04ced04bb18c9b676894ef6a7e1934ac872a354b
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-03-05 16:30:50 +0100

    Inception. Is it possible?

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,17 +1,25 @@
 #include "main.h"
 
 /**
-* is_prime_number - returns 1 if a number is a prime number or 0 if not
+* is_prime_helper - cheks if the number is prime
 * @n: number to be checked
 *
 * Return: 1 if n is a prime number otherwise 0
 */
 
-int check_prime(int n, int i);
-
-int is_prime_number(int n)
+int is_prime_helper(int n, int i);
 {
-         return (check_prime(n, 1));
+	if (i * i > n)
+	{
+		return (1);
+	}
+
+	if (n % i == 0)
+	{
+		return (0);
+	}
+
+	return (is_prime_helper(n, i + 1));
 }
 
 /**
@@ -21,22 +29,12 @@ int is_prime_number(int n)
 * Return: integer value
 */
 
-int check_prime(int n, int i)
+int is_prime_number(int n)
 {
 	if (n <= 1)
 	{
 		return (0);
 	}
 
-	if (n % i == 0 && i > 1)
-	{
-		return (0);
-	}
-
-	if ((n / 1) < i)
-	{
-		return (1);
-	}
-
-	return (check_prime(n, i + 1));
+	return (is_prime_helper(n, 2));
 }
