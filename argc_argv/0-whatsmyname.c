a9e71dbc46dba336d1ed1b6ddc5690788b80f6ae
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-03-05 18:17:40 +0100

    It ain't what they call you, it's what you answer to

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* main - returns an integer
+* @argc: count of the arguments of the programm
+* @argv: array of pointer to the strings that are argc arguments
+*
+* Return: Always 0
+*/
+int main(int argc, char *argv[])
+{
+	argc = file_name
+
+	_putchar(argv[0]);
+
+	_putchar('\n');
+
+	return (0);
+}
