644e87838150d0942a229186941e8826010c714f
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-03-05 20:26:56 +0100

    Neither irony nor sarcasm is argument

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,22 +11,21 @@
 
 int main(int argc, char *argv[])
 {
-	int i, mul;
+	int num1 = 0, num2 = 0;
 
-	mul = 0;
+	if (argc == 3)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
 
-	if (argc != 2)
+		printf("%d\n", num1 * num2);
+
+	}
+
+	else
 	{
 		printf("Error\n");
 		return (1);
-	}
-
-	for (i = 1; i < argc; i++)
-	{
-
-		mul = (atoi(argv[i])) * (atoi(argv[2]));
-
-		printf("%d\n", mul);
 	}
 
 	return (0);
