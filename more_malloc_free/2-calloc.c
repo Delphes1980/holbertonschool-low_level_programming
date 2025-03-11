74921ab58e1dc7e3475f1c3a41d40713a13f0c34
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-03-11 14:47:40 +0100

    _calloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _calloc - allocates memory for an array
+* @size: width of the array
+* @nmemb: height of the array
+*
+* Return: to a pointer of a 2 dimensioanl array of integers
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i, j;
+	void *cal;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	cal = malloc(nmemb * size);
+
+	if (cal == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < nmemb; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			cal[i][j] = 0;
+		}
+	}
+}
