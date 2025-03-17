3a69e6e095a88f2bf47b255c47778e7537c2ee1a
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-03-17 15:49:06 +0100

    To hell with circumstances; I create opportunities

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -18,20 +18,17 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	}
 
-	if (size != '0' && array != NULL && cmp != NULL)
+	if (array != NULL && cmp != NULL)
 	{
 		for (i = 0; i < size; i++)
 		{
-			cmp(array[i]);
+			if (cmp(array[i]) != 0)
+			{
+				return (i);
+			}
+
 		}
-
-		return (0);
 	}
 
-	else
-	{
-		return (-1);
-	}
-
-	return (array[i]);
+	return (-1);
 }
