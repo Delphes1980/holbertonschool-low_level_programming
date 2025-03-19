b06e92a9bfaf5adb62061322ead125282fe4413a
Coutouly-Laborda Delphine <Delphes1980>
2025-03-19 09:52:25 +0100

    pleasing vscode

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,17 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+* print_all - function that prints anything
+* @format: list of types of arguments passed to the function
+*
+* Return: (nil) if the string is NULL otherwise prints anything
+*/
+
+void print_all(const char * const format, ...)
+{
+
+
+	printf("\n");
+}
