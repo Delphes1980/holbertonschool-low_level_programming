2c4f98de6f631126d2cbf1c4ab2a5a414711020b
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-03-31 14:08:37 +0200

    create readme file + 0-print_list.c file

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* print_list - function that prints all the elements
+* @h: head of the singly linked list
+*
+* Return: all the elements of the singly linked list
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
