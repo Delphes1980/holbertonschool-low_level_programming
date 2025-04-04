e813d8fea160881bf5a6fe8d5edf8e976133cc3e
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-04-04 16:16:04 +0200

    delete unnecessary files and create 1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -48,12 +48,15 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	written_text = write(file, text_content, _strlen(text_content));
+	if (text_content != NULL)
+	{
+		written_text = write(file, text_content, _strlen(text_content));
+	}
 
 	if (written_text == -1)
 	{
 		close(file);
-		return (1);
+		return (-1);
 	}
 
 	close(file);
